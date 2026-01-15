370e4808178ff1ee8edfe651c31d9bdcc7f9d90e
thevasugupta <thevasugupta04@gmail.com>
2026-01-15 19:41:26 +0530

    http issue

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -7,10 +7,14 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
+
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
