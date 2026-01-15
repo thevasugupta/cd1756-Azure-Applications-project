bce58b520dd591cda7c4b8e5414263a9a6e48880
thevasugupta <thevasugupta04@gmail.com>
2026-01-15 23:56:01 +0530

    invalid login logging added

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -66,6 +66,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
+            app.logger.warning("Invalid login attempt")
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
